68872896352d8de40a5f9d9ac38730bb87018ad3
SnehaJarsania <sneha.jarsania@capgemini.com>
2020-08-21 15:55:35 +0530

    Initial Commit

---
+++ SJSiriDemoLib/SJSiriDemoLib.h
@@ -0,0 +1,19 @@
+//
+//  SJSiriDemoLib.h
+//  SJSiriDemoLib
+//
+//  Created by Sneha Jarsania on 8/21/20.
+//  Copyright © 2020 Sneha Jarsania. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SJSiriDemoLib.
+FOUNDATION_EXPORT double SJSiriDemoLibVersionNumber;
+
+//! Project version string for SJSiriDemoLib.
+FOUNDATION_EXPORT const unsigned char SJSiriDemoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SJSiriDemoLib/PublicHeader.h>
+
+
